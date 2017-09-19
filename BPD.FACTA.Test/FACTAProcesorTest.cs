e6c83d205e4a33264a37460efbf6cc5d2fe36d26
Ruben Ruben <ruben.tejada@gmail.com>
2017-09-19 17:11:46 -0400

    Adding Unit Testing

--- BPD.FACTA.Test/FACTAProcesorTest.cs
+++ BPD.FACTA.Test/FACTAProcesorTest.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using BPD.FACTA.Procesor;
+using BPD.FACTA.Interfaces;
+using Moq;
+
 
 namespace BPD.FACTA.Test
 {
     public class FACTAProcesorTest
     {
+
+        [Fact]
+        public void Throws_ArgumentNullException_With_Null_IFACTADataProvider()
+        {
+            //Arrange
+            var mockFACTADataProvider = new Moq.Mock<IFACTADataProvider>();            
+            var mockFACTAParser = new Moq.Mock<IFACTAParser>();
+            var mockFACTAFileGenerator = new Moq.Mock<IFACTAFileGenerator>();
+
+            //ACT
+
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => new FACTAProcesor(mockFACTADataProvider.Object, mockFACTAParser.Object, mockFACTAFileGenerator.Object));
+
+
+        }
 
 
     }
