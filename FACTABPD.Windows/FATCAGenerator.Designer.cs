1acc416144f6f2ec9932d2a34fe51b4dc17a500a
Ruben Ruben <ruben.tejada@gmail.com>
2017-09-25 16:48:52 -0400

    Log Management

--- FACTABPD.Windows/FATCAGenerator.Designer.cs
+++ FACTABPD.Windows/FATCAGenerator.Designer.cs
@@ -32,12 +32,15 @@
             this.lblOutputInfo = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.listapplicationLog = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblInputInfo
             // 
             this.lblInputInfo.AutoSize = true;
-            this.lblInputInfo.Location = new System.Drawing.Point(37, 24);
+            this.lblInputInfo.Location = new System.Drawing.Point(37, 108);
             this.lblInputInfo.Name = "lblInputInfo";
             this.lblInputInfo.Size = new System.Drawing.Size(265, 13);
             this.lblInputInfo.TabIndex = 0;
@@ -46,7 +49,7 @@
             // lblOutputInfo
             // 
             this.lblOutputInfo.AutoSize = true;
-            this.lblOutputInfo.Location = new System.Drawing.Point(37, 54);
+            this.lblOutputInfo.Location = new System.Drawing.Point(37, 135);
             this.lblOutputInfo.Name = "lblOutputInfo";
             this.lblOutputInfo.Size = new System.Drawing.Size(236, 13);
             this.lblOutputInfo.TabIndex = 1;
@@ -54,7 +57,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(202, 96);
+            this.button1.Location = new System.Drawing.Point(202, 182);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(111, 23);
             this.button1.TabIndex = 2;
@@ -66,24 +69,47 @@
             // 
             this.listapplicationLog.FormattingEnabled = true;
             this.listapplicationLog.HorizontalScrollbar = true;
-            this.listapplicationLog.Location = new System.Drawing.Point(12, 139);
+            this.listapplicationLog.Location = new System.Drawing.Point(12, 225);
             this.listapplicationLog.Name = "listapplicationLog";
             this.listapplicationLog.ScrollAlwaysVisible = true;
             this.listapplicationLog.Size = new System.Drawing.Size(490, 225);
             this.listapplicationLog.TabIndex = 3;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BPD.FATCA.Windows.Properties.Resources.Popular;
+            this.pictureBox1.Location = new System.Drawing.Point(1, -54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(194, 129);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(223, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Generador de Archivo FATCA";
+            // 
             // FATCAGenerator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(520, 376);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(517, 462);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.listapplicationLog);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lblOutputInfo);
             this.Controls.Add(this.lblInputInfo);
             this.Name = "FATCAGenerator";
-            this.Text = "Generador FATCA";
+            this.Text = "Generador FATCA BPD";
             this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -95,6 +121,8 @@
         private System.Windows.Forms.Label lblOutputInfo;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listapplicationLog;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- FACTABPD.Windows/Properties/Resources.Designer.cs
+++ FACTABPD.Windows/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace BPD.FATCA.Windows.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Popular {
+            get {
+                object obj = ResourceManager.GetObject("Popular", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
