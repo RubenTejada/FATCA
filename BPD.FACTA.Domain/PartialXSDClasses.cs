5ca93f9acfd6c391d9e27b8a5cd2a715e311605d
Ruben Ruben <ruben.tejada@gmail.com>
2017-09-28 11:02:21 -0400

    Prueba TeamCity

--- BPD.FACTA.Domain/PartialXSDClasses.cs
+++ BPD.FACTA.Domain/PartialXSDClasses.cs
@@ -5,5 +5,5 @@ public partial class FATCA_OECD
 
     [XmlAttributeAttribute("schemaLocation", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
     public string xsiSchemaLocation = "urn:oecd:ties:fatca:v2 FatcaXML_v2.0.xsd";
-
+    
 }
